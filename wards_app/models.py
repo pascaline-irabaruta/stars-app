da036ba6e523a52ac66bbf0060db733b5c00a568
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-18 21:44:48 +0200

    imported all the libraries to be used in models.py

--- wards_app/models.py
+++ wards_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericRelation
+from django.core.urlresolvers import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import Q
+from star_ratings.models import Rating
+from tinymce.models import HTMLField
+from pyuploadcare.dj.models import ImageField
+import statistics
 
 # Create your models here.
