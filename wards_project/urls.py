2045327b1d773b0bde5ae22ef051cb920582ead8
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 19:05:21 +0200

    updated urls.py for our project

--- wards_project/urls.py
+++ wards_project/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'', include('wards_app.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
 ]
