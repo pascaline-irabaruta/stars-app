850f983183f360d8ad7a10ce87a9cb49a6225331
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 10:49:42 +0200

    created a view class fora project form

--- wards_app/forms.py
+++ wards_app/forms.py
@@ -2,3 +2,21 @@ from django import forms
 from .models import Project, Profile, Review
 from tinymce.widgets import TinyMCE
 from pyuploadcare.dj.forms import ImageField
+
+SCORES = [
+    (1,1),(2,2),
+    (3,3),(4,4),
+    (5,5),(6,6),
+    (7,7),(8,8),
+    (9,9),(10,10),
+]
+class ProjectForm(forms.ModelForm):
+    project_pic = ImageField(label='')
+    class Meta:
+        model = Project
+        fields = ("title", "description", "project_pic", "live_site",)
+        widgets = {
+            "title":forms.TextInput(attrs={"class":"form-control mb-4"}),
+            "description":TinyMCE(attrs={'cols': 116, 'rows': 15}),
+            "live_site":forms.URLInput(attrs={"class":"form-control mb-4"}),
+        }
