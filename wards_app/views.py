59aa9605a6e3d2cedadf4b5eb7361c8742096392
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 11:02:22 +0200

    added all imports in views.py

--- wards_app/views.py
+++ wards_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import (TemplateView, ListView, DetailView,CreateView, UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from .models import Project, Review
+from django.contrib.auth.models import User
+from .forms import ProjectForm, ReviewForm, UpdateProfileForm
+from django.utils import timezone
+from django.urls import reverse_lazy
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProjectSerializer, UserSerializer
 
 # Create your views here.
