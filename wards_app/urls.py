1a115ff264c9b72abb165709281748b8a5f19cdf
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 19:20:36 +0200

    save changes

--- wards_app/urls.py
+++ wards_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from premios_app import views
+from wards_app import views
 from django.conf import settings
 from django.conf.urls.static import static
 
--- wards_project/settings.py
+++ wards_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'premios_app',
+    'wards_app',
     'star_ratings',
     'bootstrap4',
     'tinymce',
