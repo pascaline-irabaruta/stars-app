5fe363bf74b4bc35f5c53702c122182afe5c840b
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 19:31:35 +0200

    created a class to test project model

--- wards_app/tests.py
+++ wards_app/tests.py
@@ -20,3 +20,29 @@ class ProfileTest(TestCase):
         self.new_profile.save()
         users = User.objects.all()
         self.assertTrue(len(users)>0)
+
+class ProjectTest(TestCase):
+
+    def setUp(self):
+        self.new_user = User(username="pascy", email="pascy@gmail.com",
+                             password="pascy12345")
+        self.new_user.save()
+        self.new_profile = Profile(user=self.new_user, bio="hey there!")
+        self.new_profile.save()
+        self.new_project = Project(author=self.new_user, title=" Title",
+                                   description=" description", live_site="gmail.com")
+        self.new_project.save()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_project, Project))
+
+    def test_save_project(self):
+        self.new_project.save()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)>0)
+
+    def test_search_project(self):
+        self.new_project.save()
+        searched_project = Project.search_projects("Title")
+        self.assertTrue(len(searched_project)==1)
