538705259313f483b38218b7080b4ac0a17e1620
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-20 10:55:31 +0200

    created a serializer class for project

--- wards_app/serializers.py
+++ wards_app/serializers.py
@@ -8,3 +8,10 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ("id", "comment", "design_score", "usability_score",
                   "content_score",)
+
+class ProjectSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Project
+        fields = ("id", "author", "title", "description", "publish_date",
+                  "project_pic", "live_site", "reviews",) # to add author and reviews
