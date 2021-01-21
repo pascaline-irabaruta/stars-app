581399ccdc2ef9a586bf7339bca242464c3346a2
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-01-21 20:10:15 +0200

    ade changes

--- wards_project/settings.py
+++ wards_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['https://wards-app.herokuapp.com/']
 
 
 # Application definition
